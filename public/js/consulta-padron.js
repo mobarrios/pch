c5f2c2186d3200c3a3d0dca1180e392fcfbca41d
Manuel Barrios <mbarrios@Manuels-MBP.fibertel.com.ar>
2020-01-19 18:24:52 -0300

    up

--- public/js/consulta-padron.js
+++ public/js/consulta-padron.js
@@ -6,11 +6,11 @@ $(function(){
         grecaptcha.ready(() => {
 
             grecaptcha.execute('6Ld0idAUAAAAAFjqrXLvjzfgu2PV9tjUstBopjwS', {action: 'homepage'}).then((token) => {
-                console.log(token)
+                
                 $("#formulario button[type=submit]").insertBefore("<input type='hidden' name='token' value='" + token + "'/>");
                 $("#formulario button[type=submit]").insertBefore("<input type='hidden' name='action' value='homepage'/>");
                 
-                // $("#formulario").submit();
+                $("#formulario").submit();
             });
 
         });
