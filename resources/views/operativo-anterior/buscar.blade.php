8595503097a1e6d25f6108c04071dcf4b1264d3e
root <root@ip-172-31-5-212.sa-east-1.compute.internal>
2020-01-19 21:10:49 +0000

    up

--- resources/views/operativo-anterior/buscar.blade.php
+++ resources/views/operativo-anterior/buscar.blade.php
@@ -36,7 +36,15 @@
         }
 
     </style>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116404797-8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-116404797-8');
+</script>
 
 </head>
 <body>
